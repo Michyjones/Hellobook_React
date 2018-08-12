dbbe335d86d315ca46ba28e71274db1b77c00125
Michyjones <mykembuguah11@gmail.com>
2018-08-12 12:46:33 +0300

    [Fix] Fix the linting errors on the app file

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ import "font-awesome/css/font-awesome.css";
 import Register from "./components/RegisterForm";
 import Login from "./components/LoginForm";
 import { Switch, BrowserRouter as Router, Route} from "react-router-dom";
-// import {PrivateRoute} from "react-router";
-import {Provider} from 'react-redux';
-import {store }from './store';
+import {Provider} from "react-redux";
+import {store }from "./store";
 import Views from "./components/viewbooks";
 import Home from "./components/home";
-import addbook from "./components/addbook"
-import editbook from "./components/editbook"
+import addbook from "./components/addbook";
+import editbook from "./components/editbook";
 import singleBook from "./components/singleBook";
 import History from "./components/History";
 import NoMatch from "./components/NoMatch";
@@ -27,28 +26,28 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-          <div>
-            {/*<Header />*/}
-            <Router>
-              <div>
-                <Switch>
+        <div>
+          {/*<Header />*/}
+          <Router>
+            <div>
+              <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/register" component={Register} />
                 <Route path="/login" component={Login} />
                 <Route path="/view" component={Views}/>
                 <Route path="/home" component={Home}/>
-                  <Route path="/addbook" component={addbook}/>
-                  <Route path="/editbook/:id" component={editbook}/>
-                  <Route exact path="/book/:id" component={singleBook}/>
-                  <Route path="/history" component={History}/>
-                  <Route path="/requestpassword" component={RequestPassword}/>
-                  <Route path="/changepassword" component={PasswordReset}/>
-                  <Route path="*" component={NoMatch}/>
+                <Route path="/addbook" component={addbook}/>
+                <Route path="/editbook/:id" component={editbook}/>
+                <Route exact path="/book/:id" component={singleBook}/>
+                <Route path="/history" component={History}/>
+                <Route path="/requestpassword" component={RequestPassword}/>
+                <Route path="/changepassword" component={PasswordReset}/>
+                <Route path="*" component={NoMatch}/>
                   
               </Switch>
-              </div>
-            </Router>
-          </div>
+            </div>
+          </Router>
+        </div>
       </Provider>
     );
   }
