bf8279d42955d681728dd5e7d32a5b80c7629ec5
Michyjones <mykembuguah11@gmail.com>
2018-08-12 12:59:32 +0300

    [Fix] Fix the linting error on the tests files

--- src/tests/components/NoMatch.test.js
+++ src/tests/components/NoMatch.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { store } from '../../store/index';
-import NoMatch from '../../components/NoMatch';
+import React from "react";
+import { shallow } from "enzyme";
+import { store } from "../../store/index";
+import NoMatch from "../../components/NoMatch";
 
-describe('Page not found component', () => {
-  it('Renders 404 error component', () => {
+describe("Page not found component", () => {
+  it("Renders 404 error component", () => {
     const wrapper = shallow(<NoMatch store={store} />);
     expect(wrapper).toHaveLength(1);
   });
